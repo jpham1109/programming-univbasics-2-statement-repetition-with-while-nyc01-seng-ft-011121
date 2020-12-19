ad311055b95cebfbf9dd896973ad315199dd78d1
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2020-12-19 21:02:16 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,10 +1,10 @@
 # Write your code here
 count = 10 
-while (count > 0)
+while count > 0
 do
   puts count
   count -= 1
-break if count == 0 
+  break if count == 0 
   puts "Happy New Year!"
 end
   
